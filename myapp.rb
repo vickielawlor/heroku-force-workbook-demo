dad158915067a29e2e0bdbe79920f6972abce1ee
vickielawlor <112431052@umail.ucc.ie>
2015-06-02 12:12:38 +0100

    change query to query events

--- myapp.rb
+++ myapp.rb
@@ -44,7 +44,7 @@ class MyApp < Sinatra::Base
   d = #{yr}-#{mnt}-#{dy}
   get '/' do
     logger.info "Visited home page"
-    @accounts= client.query("SELECT AccountId,StartDateTime FROM Event WHERE EndDateTime =" << d << "T07:30:00.000+0000")
+    @accounts= client.query("SELECT AccountId,StartDateTime FROM Event WHERE EndDateTime =#{ "d" }T07:30:00.000+0000")
     erb :index
   end
 
