4b02acfeca50374e552ef0fa95b8c416c8d74d7b
vickielawlor <112431052@umail.ucc.ie>
2015-06-02 15:54:31 +0100

    change query to query events

--- myapp.rb
+++ myapp.rb
@@ -32,7 +32,7 @@ class MyApp < Sinatra::Base
     end
 
   end
-  
+
 
   date = Time.now
 
@@ -49,6 +49,7 @@ class MyApp < Sinatra::Base
 
 
     @accounts= client.query("SELECT AccountId,StartDateTime FROM Event WHERE EndDateTime = #{d}T07:30:00.000+0000")
+    @accounts= client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE FirstName = 'Ronan'")
     erb :index
   end
 
