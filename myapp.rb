1fb34b2d66268a9e163b5b48c89bce0530f054c3
vickielawlor <112431052@umail.ucc.ie>
2015-06-03 10:30:23 +0100

    change query to query events

--- myapp.rb
+++ myapp.rb
@@ -54,22 +54,13 @@ class MyApp < Sinatra::Base
   get '/' do
     logger.info "Visited home page"
 
-    @accounts = client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE FirstName = 'Ronan'")
+    @accounts1 = client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE FirstName = 'Ronan'")
     erb :index
   end
 
 
   get '/authenticate' do
     redirect "/auth/salesforce"
-  end
-
-  get '/auth/salesforce/callback' do
-    logger.info "#{env["omniauth.auth"]["extra"]["display_name"]} just authenticated"
-    credentials = env["omniauth.auth"]["credentials"]
-    session['token'] = credentials["token"]
-    session['refresh_token'] = credentials["refresh_token"]
-    session['instance_url'] = credentials["instance_url"]
-    redirect '/'
   end
 
   get '/auth/salesforce/callback' do
