6e09cfd41936df7e4941d3518baa9054a99d0791
vickielawlor <112431052@umail.ucc.ie>
2015-07-07 12:34:11 +0100

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -66,7 +66,7 @@ end
     @accounts1 = client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE Id = '00580000003lR2a' OR Id = '00580000003lR5B'
                     OR Id = '00580000003lQuG' OR Id = '005340000082AzV' OR Id = '00580000003muAa' ")
 
-   @accounts2 = clienttt.query("SELECT Owner.Name FROM EVENT WHERE StartDateTime>2015-07-07T07:29:00.000Z AND EndDateTime<2015-07-07T16:31:00.000Z COUNT >1 ")
+   @accounts2 = clienttt.query("SELECT Owner.Name FROM EVENT WHERE StartDateTime>2015-07-07T07:29:00.000Z AND EndDateTime<2015-07-07T16:31:00.000Z Group By Owner.name ")
     erb :index
   end
   
