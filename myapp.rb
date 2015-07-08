6c5bfed6ad9623a2786e11b2ad69bb3c19e5753a
vickielawlor <112431052@umail.ucc.ie>
2015-07-08 10:29:07 +0100

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -65,7 +65,7 @@ class MyApp < Sinatra::Base
 
     @accounts1 = client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE Id = '00580000003lR2a' OR Id = '00580000003lR5B'
                     OR Id = '00580000003lQuG' OR Id = '005340000082AzV' OR Id = '00580000003muAa' ")
-    @accounts2 = clienttt.query("SELECT owner.name from event where StartDateTime>2015-07-08T07:29:00.000Z and EndDateTime<2015-07-08T17:01:00.000Z and owner.title in ('cloud services engineer','cloud service engineer')")
+    @accounts2 = clienttt.query("SELECT owner.name from event where StartDateTime>2015-07-08T07:29:00.000Z and EndDateTime<2015-07-08T17:01:00.000Z and owner.title in ('cloud services engineer','cloud service engineer') and showas= 'busy'")
     erb :index
   end
 
